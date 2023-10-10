3406fe5fcc41d57aafb32f4c8d868224a8dad810
Gia Khang <khanggia85@gmail.com>
2023-10-10 20:33:28 +0700

    fixed bugs

--- src/Components/InputZone/GeneratedShortenLink.js
+++ src/Components/InputZone/GeneratedShortenLink.js
@@ -1,31 +1,35 @@
-import {message} from 'antd'
+import { message } from "antd";
 
 //generate link
 export const GenerateShortenLink = (generateUrl) => {
-    //get input url & path
-    const SelectedInput = document.querySelectorAll(".ant-input");
-    const userInputUrl = SelectedInput[0].value;
-    const userInputPath = SelectedInput[1].value;
+  //get input url & path
+  const SelectedInput = document.querySelectorAll(".ant-input");
+  const userInputUrl = SelectedInput[0].value;
+  const userInputPath = SelectedInput[1].value;
 
-    //check if user is using path or not
-    //not using path
-    let data = {
-      domain: "shorten.giakhang3005.com",
-      originalURL: userInputUrl,
-      allowDuplicates: false,
-    };
-    if (userInputPath.length === 0) {
-      //success
-      generateUrl(data, userInputUrl);
-      //using path, check for length
-    } else if (userInputPath.length >= 5 && userInputPath.length <= 15) {
-      data = {
-        ...data,
-        path: userInputPath,
-      };
-      //success
-      generateUrl(data, userInputUrl);
-    } else {
-      message.error("Path must between 5 and 15 characters");
-    }
+  let inDup = false;
+
+  //check if user is using path or not
+  //not using path
+  let data = {
+    domain: "shorten.giakhang3005.com",
+    originalURL: userInputUrl,
+    allowDuplicates: false,
   };
+
+  //not using custom path
+  if (userInputPath.length === 0) {
+    //success -> use function of the hook
+    generateUrl(data, userInputUrl);
+    //using path, check for length
+  } else if (userInputPath.length >= 5 && userInputPath.length <= 15) {
+    data = {
+      ...data,
+      path: userInputPath,
+    };
+    //success -> use function of the hook
+    generateUrl(data, userInputUrl);
+  } else {
+    message.error("Path must between 5 and 15 characters");
+  }
+};
